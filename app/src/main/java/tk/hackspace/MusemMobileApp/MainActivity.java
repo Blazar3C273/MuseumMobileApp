a5279cccb3b6cb8c9e6e0cfe788c62dc2fd787f4
Stepanenko Anatoliy <tolik.stepanenko@gmail.com>
2015-01-17 18:04:16 +0300

    pre holiday version

---
+++ app/src/main/java/tk/hackspace/MusemMobileApp/MainActivity.java
@@ -0,0 +1,14 @@
+package tk.hackspace.MusemMobileApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Tolik on 13.12.2014.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
